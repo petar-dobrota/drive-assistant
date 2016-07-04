d4fd8a3829c6a43f1d27822d8eb97cbc2f6883c5
Petar Dobrota <petar.dobrota@gmail.com>
2016-07-04 23:06:17 +0200

    rpmToThrottle function implementation

--- drive-assistant.ino
+++ drive-assistant.ino
@@ -29,6 +29,12 @@ const unsigned NUM_GEARS = 5;
 const float GEAR_RATIOS[NUM_GEARS] = {98.4252f, 59.5613f, 40.1856f, 30.6805f, 23.5658f}; // gearRatio=rpm/v
 const float GEAR_RATIO_TOLERANCE = 0.15f; // gear ratio tolerance when determinating in which gear car is 
 
+// rpmToThrottle function
+const int rpmToThrottleDomain = 7000;
+const int rpmToThrottleN = 120;
+// TODO: add actual data
+const float rpmToThrottleFunction[rpmToThrottleN] = {1.2f};
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // GLOBALS
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -63,8 +69,25 @@ void setup()
   digitalWrite(TEST_1_PIN, LOW);
 }
 
+// Determinates how much preasure throttle pedal needs to reach specified RPM when engine isn't loaded.
 float rpmToThrottle(int rpm) {
-  return 0;
+  // rpm on X, throttle on Y
+  
+  float xStep = (float) rpmToThrottleDomain / rpmToThrottleN;
+
+  float mx = rpm / xStep;
+  int lx = (int) mx;
+  int hx = lx + 1;
+
+  if (hx < rpmToThrottleN) {
+    float ly = rpmToThrottleFunction[lx];
+    float hy = rpmToThrottleFunction[hx];
+
+    return ((mx - lx) * (hy - ly) / (hx - lx)) + ly;
+  } else {
+    // desired rpm is way to high, probably error, return 0 to not overrev the engine
+    return 0;
+  }
 }
 
 void showGear(int g) {
@@ -87,6 +110,10 @@ void showGear(int g) {
     digitalWrite(TEST_1_PIN, LOW);
     digitalWrite(TEST_2_PIN, LOW);
   }
+}
+
+void setThrottlePos(float newPos) {
+  // TODO: Also check and cap range of newPos
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -138,7 +165,7 @@ void rpmSetting(int desiredRpm) {
     out = 0;
   }
 
-  // TODO: Set throttlePos = out
+  setThrottlePos(out);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
