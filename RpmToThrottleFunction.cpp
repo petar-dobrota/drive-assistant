2b71392651764fc1b18694df5783a73ea5c458b1
Petar Dobrota <petar.dobrota@gmail.com>
2017-01-29 22:30:49 +0100

    RpmToThrottleFunction production settings

--- RpmToThrottleFunction.cpp
+++ RpmToThrottleFunction.cpp
@@ -2,35 +2,26 @@
 
 namespace RpmToThrottleFunction {
 
-const int rpmToThrottleDomain = 6000;
-const int rpmToThrottleN = 78;
-// TODO: add actual data
-const int rpmToThrottleFunction[rpmToThrottleN] = { 0, 2, 4, 6, 8, 10, 12, 14,
-		16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50,
-		52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86,
-		88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116,
-		118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144,
-		146, 148, 150, 152, 154 };
-
 // Determinates how much pressure throttle pedal needs to reach specified RPM when engine isn't loaded.
 int getThrottle(float rpm) {
-	// rpm on X, throttle on Y
 
-	float xStep = (float) rpmToThrottleDomain / rpmToThrottleN;
+	float app;
 
-	float mx = rpm / xStep;
-	int lx = (int) mx;
-	int hx = lx + 1;
-
-	if (hx < rpmToThrottleN) {
-		float ly = rpmToThrottleFunction[lx];
-		float hy = rpmToThrottleFunction[hx];
-
-		return (int) ((((mx - lx) * (hy - ly) / (hx - lx)) + ly) + 0.5f);
+	if (rpm > 7200.0f) {
+		app = 77.0f;
+	} else if (rpm > 3783.0f) {
+		app = (rpm + 14419.0f) / 281.0f;
+	} else if (rpm > 1600.0f) {
+		app = (rpm + 16143.34247f) / 311.1712329f;
+	} else if (rpm > 1000.0f) {
+		app = (rpm + 6668.75f) / 145.25f;
+	} else if (rpm > 800.0f) {
+		app = (rpm + 2692.214286f) / 69.64285714f;
 	} else {
-		// desired rpm is way to high, probably error, return 0 to not overrev the engine
-		return 0;
+		app = 40;
 	}
+
+	return (int) (app + 0.5f);
 }
 
 }
